a775af23a341daf2b391b3b052b9d62e6dc4ad2c
Eric Chiu <ericwchiu@gmail.com>
2021-02-22 14:15:06 -0800

    finished basic reqs

--- index.js
+++ index.js
@@ -40,29 +40,3 @@ app.use((err, req, res, next) => {
 app.listen(app.get('port'), () => {
     console.log('Express started on port ' + app.get('port') + '; Ctrl+C to terminate')
 })
-
-
-// // picture carousel
-// let picIndex = 1;
-// scrollPics(picIndex);
-
-// function nextPic(i) {
-//     scrollPics(picIndex += i);
-// }
-
-// function scrollPics(i) {
-//     const pics = document.getElementsByClassName("pics");
-
-//     if (i > pics.length) {
-//         picIndex = 1;
-//     }
-//     if (i < 1) {
-//         picIndex = 3;
-//     }
-
-//     for (let x = 0; x < 3; x++) {
-//         pics[x].style.display = "none"
-//     }
-
-//     pics[picIndex - 1].style.display = "block";
-// }
